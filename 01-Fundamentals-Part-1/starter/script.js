f863418804666007c1b4531730807949ba114a46
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-07-14 10:54:27 -0300

    exercise 3

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -224,3 +224,21 @@ if (hasDriverLicense && hasGoodVision && !isTired) {
 }
 */
 
+/*
+// Ejercicio 3:
+
+const scoreDolphins = (96 + 108 + 89) / 3;
+const scoreKoalas = (88 + 91 + 110) / 3;
+
+// console.log(scoreDolphins, scoreKoalas)
+
+if (scoreDolphins > scoreKoalas) {
+    console.log('Dolphins win the trophy')
+} else if (scoreKoalas > scoreDolphins) {
+    console.log('Koalas win the trophy')
+} else if (scoreKoalas === scoreDolphins) {
+    console.log('Both win the trophy')
+}
+
+*/
+
