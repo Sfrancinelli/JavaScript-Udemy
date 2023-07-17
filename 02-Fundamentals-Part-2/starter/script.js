2c17cf03fceb950855a2fccadd830cfab671111a
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-07-17 15:44:28 -0300

    Lecture 38 (exercise)

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -82,5 +82,39 @@ function fruitProcessor(apples, oranges) {
 console.log(fruitProcessor(2,3))
 */
 
+/*
 // Lecture 37
+const calcAge = (birthYear) => 2023 - birthYear;
 
+const yearsUntilRetirement = function (birthYear, firstName) {
+    const age = calcAge(birthYear);
+    const retirement = 65 - age;
+    if (retirement <= 0) {
+        return `${firstName} it's already on an age for retirement!`
+    } else {
+        return `${firstName} has ${retirement} years until retirement.`
+    }
+}
+
+console.log(yearsUntilRetirement(1950, 'Sebastián Francinelli'))
+*/
+
+/*
+// Lecture 38 (exercise)
+const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+
+const scoreDolphins = calcAverage(85, 54, 41);
+const scoreKoalas = calcAverage(23, 34, 27);
+
+function checkWinner(avgDolphins, avgKoalas) {
+    if (avgDolphins >= (avgKoalas * 2)) {
+        console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+    } else if (avgKoalas >= (avgDolphins * 2)) {
+        console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
+    } else {
+        console.log('No team wins...')
+    }
+}
+
+checkWinner(scoreDolphins, scoreKoalas)
+*/
