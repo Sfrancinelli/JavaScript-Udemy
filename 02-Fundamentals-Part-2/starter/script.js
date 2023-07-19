8d2d54ea399ae525cedc08b67cbb7cfbd5a6faf6
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-07-19 15:20:25 -0300

    lecture 50 exercise!

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -405,3 +405,33 @@ while (dice !== 6) {
     if (dice === 6) console.log('Loop is about to end...');
 }
 */
+
+// Lecture 50 / Exercise
+const calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+
+const calcAverage = function(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum =+ arr[i];
+    }
+    const avg = sum / arr.length;
+
+    return avg
+}
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+    tips.push(calcTip(bills[i]));
+    totals.push(tips[i] + bills[i]);
+    console.log(bills[i], tips[i], totals[i]);
+};
+
+console.log(totals);
+const avgTotals = calcAverage(totals);
+console.log(avgTotals);
+
