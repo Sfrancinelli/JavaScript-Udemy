04871763c996840ac8c8783a7a82b119d6333bda
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-07-21 15:44:40 -0300

    cleaning up previous code

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -6,8 +6,8 @@
 const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 
 function tempAmplitud(arr) {
-    let greaterValue = 0;
-    let lesserValue = 0; 
+    let greaterValue = temperatures[0];
+    let lesserValue = temperatures[0]; 
     for (let i = 0; i <= arr.length; i++) {
         if (typeof arr[i] !== 'number') {
             arr.splice(i, 1);
@@ -46,3 +46,22 @@ const measureKelvin = function () {
 }
 
 console.log(measureKelvin());
+
+// Optimizing the amplitud calculator:
+const calcTempAmplitude = function(t1,t2) {
+    const temps = t1.concat(t2);
+    
+    let max = temps[0];
+    let min = temps[0];
+
+    for (let i = 0; i < temps.length; i++) {
+        const currentTemp = temps[i];
+        if (typeof currentTemp !== 'number') continue;
+
+        if (currentTemp > max) max = currentTemp;
+        if (currentTemp < min) min = currentTemp;
+    }
+    console.log(max, min);
+    return max - min;
+};
+const amplitude = calcTempAmplitude([3, 5, 1], [9, 0, 5]);
