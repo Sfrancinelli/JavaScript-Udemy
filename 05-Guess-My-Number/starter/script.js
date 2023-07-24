b8eb617e10a0b934df85728fd1a6b0777fce6b77
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-07-24 09:56:48 -0300

    Added green background color on win and removing it when restart

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,55 +1,55 @@
 'use strict';
-'use strict';
 
 let randomNum = Math.trunc(Math.random() * 20) + 1;
 
 let highscore = document.querySelector('.highscore').textContent;
 let score = Number(document.querySelector('.score').textContent);
-console.log(randomNum)
+console.log(randomNum);
+let cuerpo = document.getElementsByTagName('body')[0];
+console.log(cuerpo.classList);
 
-let againFunc = function() {
-    highscore = document.querySelector('.highscore').textContent;
-    document.querySelector('.score').textContent = 20;
-    randomNum = Math.trunc(Math.random() * 20) + 1;
-    document.querySelector('.guess').value = "";
-    document.querySelector('.message').textContent = 'Start guessing...'
+let againFunc = function () {
+  highscore = document.querySelector('.highscore').textContent;
+  document.querySelector('.score').textContent = 20;
+  randomNum = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.guess').value = '';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  cuerpo.classList.remove('win');
+};
 
-}
+let checkFunc = function () {
+  highscore = document.querySelector('.highscore').textContent;
+  let input = Number(document.querySelector('.guess').value);
+  let number = document.querySelector('.number').textContent;
+  score = Number(document.querySelector('.score').textContent);
+  console.log(randomNum);
 
-let checkFunc = function() {
-
-    highscore = document.querySelector('.highscore').textContent;
-    let input = Number(document.querySelector('.guess').value);
-    let number = document.querySelector('.number').textContent;
-    score = Number(document.querySelector('.score').textContent);
-
-    console.log(randomNum)
-
-    if (input !== randomNum) {
-
-        if(input < randomNum) {
-            document.querySelector('.message').textContent = 'Too low!'
-        } else if(input > randomNum) {
-            document.querySelector('.message').textContent = 'Too high!'
-        }
-        number = input;
-        score -= 1;
-        document.querySelector('.score').textContent = score;
-        document.querySelector('.number').textContent = number;
-
-    } else if (input === randomNum) {
-
-        document.querySelector('.message').textContent = '🎉 Correct Number!'
-
-        if (Number(highscore) < score) {
-            document.querySelector('.highscore').textContent = score;
-        }
+  if (input !== randomNum) {
+    if (input < randomNum) {
+      document.querySelector('.message').textContent = 'Too low!';
+    } else if (input > randomNum) {
+      document.querySelector('.message').textContent = 'Too high!';
     }
+    number = input;
+    score -= 1;
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = number;
+  } else if (input === randomNum) {
+    cuerpo.classList.add('win');
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
 
-    return highscore;
-}
+    if (Number(highscore) < score) {
+      document.querySelector('.highscore').textContent = score;
+    }
+  }
 
-let checkBtn = document.querySelector('.check').addEventListener('click', checkFunc);
+  return highscore;
+};
 
-let againBtn = document.querySelector('.again').addEventListener('click', 
-againFunc);
+let checkBtn = document
+  .querySelector('.check')
+  .addEventListener('click', checkFunc);
+
+let againBtn = document
+  .querySelector('.again')
+  .addEventListener('click', againFunc);
