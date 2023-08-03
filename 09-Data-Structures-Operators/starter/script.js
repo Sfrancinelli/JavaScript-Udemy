baa6f98fa7aa552e770af8f0cf91ebe6769e21b6
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-08-03 15:34:51 -0300

    Starting lecture 109

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -12,6 +12,35 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  // orderDelivery: function (obj) {
+  //   console.log(obj);
+  // },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    console.log(
+      `Order reveived! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
+    );
+  },
+
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient, otherIngredients);
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -27,3 +56,245 @@ const restaurant = {
     },
   },
 };
+
+const rest1 = {
+  name: 'Capri',
+  numGuests: 20,
+};
+
+const rest2 = {
+  name: 'La Piazza',
+  owner: 'Giovanni Rossi',
+};
+
+///////////////////////////7
+// Spreand operator (...) (So usefull)
+// SPREAND BECAUSE ON RIGHT SIDE!! GO TO LINE 200 TO SEE REST (on the left)
+
+// The SPREAD OPERATOR is use where we would otherwise write values
+// separated by a comma
+/*
+const arr = [7, 8, 9];
+const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewArr);
+
+// Spread operator to do the above but fine
+const newArr = [1, 2, ...arr];
+console.log(newArr);
+
+console.log(...newArr);
+
+const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+console.log(newMenu);
+
+// Copy array
+const mainMenuCopy = [...restaurant.mainMenu];
+
+// Join 2 arrays
+const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
+console.log(menu);
+
+// Iterables: arrays, strings, maps , sets. NOT objects
+
+const str = 'Jonas';
+const srtArr = [...str, '', 'S.'];
+console.log(srtArr);
+
+// Real world example
+/*
+const ingredients = [
+  prompt("Let's make mpasta! Ingredient 1?"),
+  prompt('Ingredient 2?'),
+  prompt('Ingredient 3?'),
+];
+
+restaurant.orderPasta(ingredients[0], ingredients[1], ingredients[2]);
+restaurant.orderPasta(...ingredients);
+*/
+/*
+// Objects and spread operator (ES2018)
+const newRestaurant = { foundedIn: 1998, ...restaurant, founder: 'Guiseppe' };
+console.log(newRestaurant);
+
+const restaurantCopy = { ...restaurant };
+restaurantCopy.name = 'Ristorante Roma';
+console.log(restaurantCopy.name);
+console.log(restaurant.name);
+*/
+
+/*
+//////////////////////////////////////
+// Destructuring Objects
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via del sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+restaurant.orderDelivery({
+  address: 'Via del Sole, 21',
+  starterIndex: 0,
+});
+
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+// Changing the name of the variable that contains the same value from the object
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+// Default values
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+// Mutating variables
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+({ a, b } = obj);
+console.log(a, b);
+
+// Nested objects
+const {
+  fri: { open: o, close: c },
+} = openingHours;
+console.log(o, c);
+*/
+/*
+////////////////////////////////
+// Destructuring Arrays
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x, y, z] = arr;
+console.log(x, y, z);
+
+let [first, second] = restaurant.categories;
+console.log(first, second);
+
+const [, , three, fourth] = restaurant.categories;
+console.log(three, fourth);
+
+// Changing the order of first and second for each other
+const temp = first;
+first = second;
+second = temp;
+console.log(first, second);
+
+// Doing the same with deestructuring
+[first, second] = [second, first];
+console.log(first, second);
+
+console.log(restaurant.order(2, 0));
+
+// Receive 2 return values from a function
+const [starter, main] = restaurant.order(2, 0);
+console.log(starter, main);
+
+// Nested deestructuring
+const nested = [2, 4, [5, 6]];
+const [nest1, nest2, nest56] = nested;
+const [five, six] = nest56;
+console.log(nest1, nest2, nest56, five, six);
+
+// or, to the the above in one step:
+const [i, j, [k, l]] = nested;
+// Deestructuring inside a deestructuring
+console.log(i, j, k, l);
+
+// Default values
+const [p = 1, q = 1, r = 1] = [8, 9];
+// If we dont default the values, the r value will ve undefined cause theres nothing on the index "2"
+console.log(p, q, r);
+*/
+
+// REST SINTAX (...) on the left of the assign operator
+// The REST operator
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
+
+const [pizza, , risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+console.log(pizza, risotto, otherFood);
+
+// The rest operator needs to be the last one cause it will acumulate all the
+// Values that have been left behind. It can only be one rest operator
+
+// The REST OPERATOR is used where we otherwise would write variables
+// separated by a commma
+
+// Objects
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(sat, weekdays);
+
+// Functions
+// REST ARGUMENTS
+const add = function (...numbers) {
+  console.log(numbers);
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  console.log(sum);
+  return sum;
+};
+add(2, 3);
+add(2, 4, 6, 7, 8, 9, 4, 3, 2, 1);
+
+const numbers = [23, 5, 7, 8];
+add(...numbers);
+
+restaurant.orderPizza(
+  'Mozzarella',
+  'Mushrooms',
+  'Cipolla',
+  'Olives',
+  'Sinach',
+  'Ham'
+);
+
+restaurant.orderPizza('Mozzarella');
+
+console.log('-------OR-------');
+// Logical operator can use ANY data type, return ANY data type, short-circuiting
+// The OR operator shortcircuits when it meets the first thruthy value
+console.log(3 || 'Jonas');
+console.log(false || 'Jonas');
+console.log('' || 'Jonas');
+console.log(true || 0);
+console.log(undefined || null);
+console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+
+restaurant.numGuests = 0;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+
+console.log('-------AND-------');
+// The OR operator shortcircuits when it meets the first falsy value
+console.log(0 && 'Jonas');
+console.log(7 && 'Jonas');
+console.log('Hello' && 23 && null && 'jonas');
+
+if (restaurant.orderPizza) {
+  restaurant.orderPizza('Mushrooms', 'spinach');
+}
+
+restaurant.orderPizza && restaurant.orderPizza('Mushrooms', 'spinach');
+
+console.log('-------Nullish coalescing operator-------');
+// Nullish values = null && undefined (NOT 0 or '')
+// The OR operator shortcircuits when it meets the first thruthy value
+const guestCorrect = restaurant.numGuests ?? 10;
+console.log(guestCorrect);
