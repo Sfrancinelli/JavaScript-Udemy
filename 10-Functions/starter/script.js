3ba8b698fc4bff09badd84dbf68a8dbdbcb5fb04
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-08-14 12:05:52 -0300

    Lecture 135, coding challenge for functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -241,3 +241,80 @@ console.log(tax(0.1)(200));
 const addVAT2 = tax(0.23);
 
 console.log(addVAT2(200));
+
+/*
+Let's build a simple poll app! 
+A poll has a question, an array of options from which people can choose, and an 
+array with the number of replies for each option. This data is stored in the starter 
+'poll' object below. 
+Your tasks: 
+1. Create a method called 'registerNewAnswer' on the 'poll' object. The 
+method does 2 things: 
+1.1. Display a prompt window for the user to input the number of the 
+selected option. The prompt should look like this: 
+What is your favourite programming language? 
+0: JavaScript 
+1: Python 
+2: Rust 
+3: C++ 
+(Write option number) 
+1.2. Based on the input number, update the 'answers' array property. For 
+example, if the option is 3, increase the value at position 3 of the array by 
+1. Make sure to check if the input is a number and if the number makes 
+sense (e.g. answer 52 wouldn't make sense, right?) 
+2. Call this method whenever the user clicks the "Answer poll" button.
+
+3. Create a method 'displayResults' which displays the poll results. The 
+method takes a string as an input (called 'type'), which can be either 'string' 
+or 'array'. If type is 'array', simply display the results array as it is, using 
+console.log(). This should be the default option. If type is 'string', display a 
+string like "Poll results are 13, 2, 4, 1".  
+
+4. Run the 'displayResults' method at the end of each 
+'registerNewAnswer' method call. 
+
+5. Bonus: Use the 'displayResults' method to display the 2 arrays in the test 
+data. Use both the 'array' and the 'string' option. Do not put the arrays in the poll 
+object! So what should the this keyword look like in this situation? 
+ 
+ 
+The Complete JavaScript Course 21 
+Test data for bonus:  
+§ Data 1: [5, 2, 3] 
+§ Data 2: [1, 5, 3, 9, 6, 1] 
+Hints: Use many of the tools you learned about in this and the last section 😉 
+ 
+GOOD LUCK 😀 
+*/
+
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3:  C++'],
+  // This generates [0, 0, 0, 0]. More in the next section!
+  answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    const input = Number(
+      prompt(
+        'What is your favourite programming language?\n0: JavaScript\n1: Python\n2: Rust\n3: C++\n(Write option number)'
+      )
+    );
+    console.log(input);
+    if (input <= 3 && input >= 0) this.answers[input]++;
+    else alert('Please answer with a proper number');
+    this.displayResults('string');
+  },
+  displayResults(type = 'array') {
+    return type === 'array'
+      ? console.log(this.answers)
+      : console.log(`Poll results are ${this.answers.join(', ')}`);
+    // console.log(`Poll results are ${this.answers[0]}, ${this.answers[1]}, ${this.answers[2]}, ${this.answers[3]}`)
+  },
+};
+
+const answerBtn = document.querySelector('.poll');
+answerBtn.addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+// Manually setting this keyword to be the bonus data array
+// Test data for bonus:
+// § Data 1: [5, 2, 3]
+poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
