9fa324ee63a30791b5d9e2933ef7bd093a94fe2c
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-08-17 09:54:54 -0300

    displayMovements function

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -229,4 +229,22 @@ const login = function () {
   return user;
 };
 
+const displayMovements = function (movements) {
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `
+<div class="movements__row">
+<div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+<div class="movements__date">3 days ago</div>
+<div class="movements__value">${mov}</div>
+</div>
+`;
+
+    containerMovements.insertAdjacentHTML('beforeend', html);
+  });
+};
+
+displayMovements(account1.movements);
+
 btnLogin.addEventListener('click', login);
