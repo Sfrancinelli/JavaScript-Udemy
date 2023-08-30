1f1f6ee76343ca60ed30abd58b7f6774a4e4c192
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-08-30 15:17:52 -0300

    Lecture 171. Math methods!

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -94,7 +94,7 @@ const displayMovements = function (movements, sort = false) {
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${mov}€</div>
+        <div class="movements__value">${mov.toFixed(2)}€</div>
       </div>
     `;
 
@@ -104,19 +104,19 @@ const displayMovements = function (movements, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance}€`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`;
+  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)}€`;
+  labelSumOut.textContent = `${Math.abs(out).toFixed(2)}€`;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -126,7 +126,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 };
 
 const createUsernames = function (accs) {
@@ -206,7 +206,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = Number(inputLoanAmount.value);
+  const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
@@ -280,3 +280,38 @@ console.log(Number.isFinite(23 / 0)); // false
 
 // Check for integer
 console.log(Number.isInteger(20)); // true
+
+// square root
+console.log(Math.sqrt(25)); // 5
+console.log(25 ** (1 / 2));
+
+// Cubic root
+console.log(8 ** (1 / 3));
+
+// Return the maximum and minimum value
+console.log(Math.max(5, 18, 23, 11, 2)); // 23
+console.log(Math.min(5, 18, 23, 11, 2)); // 2
+
+// Calculate area of a circule with radius of 10px
+console.log(Math.PI * Number.parseFloat('10px') ** 2);
+
+// Random numbers
+console.log(Math.trunc(Math.random() * 6) + 1);
+
+// Function to get random Ints beetween values passed
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min) + 1) + min;
+
+// Rounding integers
+console.log(Math.trunc(23.3)); // Removes decimal part
+console.log(Math.round(23.9)); // Round to the nearest integer
+console.log(Math.ceil(23.3)); // Rounds always to the next integer (in this case, the result is 24)
+console.log(Math.floor(23.9)); // Always rounded down (in this case 23)
+
+// With negatives, trunc and floor works different
+console.log(Math.trunc(-23.3)); // Removes decimal part
+console.log(Math.floor(-23.9)); // Always rounded down (in this case -24)
+
+// Rounding decimals
+console.log((2.7).toFixed()); // Returns string (in this case, 3)
+console.log((2.7).toFixed(3)); // Returns string (in this case, 2.700)
