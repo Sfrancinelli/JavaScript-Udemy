e3f6b9f0c1de6cb31591be255de9d325ea1ecb34
unknown <sebastianfrancinelli00@gmail.com>
2023-09-04 12:41:21 -0300

    Continue lecture 179 about formatting numbers

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -507,3 +507,22 @@ const calcDaysPassed = (date1, date2) =>
 
 const days1 = calcDaysPassed(new Date(2037, 3, 14), new Date(2037, 3, 24));
 console.log(days1);
+
+// Internationalizing Numbers (Intl)
+const num = 3884764.23;
+
+const options = {
+  // style: 'unit',
+  // unit: 'celsius',
+  // style: 'percent',
+  style: 'currency',
+  currency: 'USD',
+};
+
+console.log('US:', new Intl.NumberFormat('en-US', options).format(num));
+console.log('Germany:', new Intl.NumberFormat('de-DE', options).format(num));
+console.log('Syria:', new Intl.NumberFormat('ar-SY', options).format(num));
+console.log(
+  'Browser:',
+  new Intl.NumberFormat(navigator.language, options).format(num)
+);
