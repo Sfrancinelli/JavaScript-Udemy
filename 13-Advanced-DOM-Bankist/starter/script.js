9d59957a0daa2fe7f1b1880c037f36a3433ac885
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-09-06 11:37:21 -0300

    Lecture 186. Selecting, Creating and Deleting elements

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -29,3 +29,59 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+//////////////////////////////////////////////////////////////////////
+// LECTURES
+// Selecting the entire document, the head and the body
+// console.log(document.documentElement);
+// console.log(document.head);
+// console.log(document.body);
+
+// Selecting by class or ID
+const header = document.querySelector('.header');
+// QuerySelectorAll creates a nodelist if more than one element is found. It is possible to use forEach method on nodelists
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+// Getting element by ID (selecting without the selector, there's no need to wirte, in this case, #section--1)
+document.getElementById('section--1');
+// Getting elements by its HTML tag -- returns an HTMLCollection (it updates inmediately if the DOM changes)
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+// Getting element by className. Same as the ID selector, its not necessary to use the CSS selector as an argument. Simply the name of the class. It also return an HTMLCollection and not a NodeList!
+console.log(document.getElementsByClassName('btn'));
+
+// Creating and inserting elements
+// .insertAdjacentHTML
+
+// Creating a cookie message programaticly and inserting it to the DOM
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent =
+//   'We use cookies for imporoved functionality and analytics.';
+// Creating the HTML for the cookie message object
+message.innerHTML =
+  'We use cookies for imporoved functionality and analytics. <button class="btn btn--close--cookie">Got it!<button>';
+
+// the Prepend method adds the element as the first child of the selected element. In this case, as the first child of the header
+header.prepend(message);
+// the append method adds its as the last child of the selected element
+header.append(message);
+// One element can only be in one side of the DOM at a time. To append multiple copies of the element into the DOM we have to indeed, copy it.
+// To copy an element we use cloneNode(true)
+// header.prepend(message.cloneNode(true));
+// After this line we have a copy of the message prepended and it looks like the same element its in both places, but in reallity its a copy.
+
+// The before method will insert the element before the selected object (in this case, the message element will be added before the header even starts)
+// header.before(message.cloneNode(true))
+// The after method will to the ssame as before() but after the selected element
+// header.after(message.cloneNode(true));
+
+// Deleting elements
+document
+  .querySelector('.btn--close--cookie')
+  .addEventListener('click', function () {
+    // The remove method is new, this was done as below
+    message.remove();
+    //message.parentElement.removeChild(message)
+  });
