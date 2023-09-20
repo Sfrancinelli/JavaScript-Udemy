665a9044c12c61450917ed5bf138eff69273d183
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-09-20 15:23:32 -0300

    Lecture 209. Prototypes

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -7,9 +7,9 @@ const Person = function (firstName, birthYear) {
   this.birthYear = birthYear;
 
   // Instance methods -- Bad practice
-  this.calcAge = function () {
-    console.log(2037 - this.birthYear);
-  };
+  //   this.calcAge = function () {
+  //     console.log(2037 - this.birthYear);
+  //   };
 };
 
 // The constructor functions are always called with the 'new' operator.
@@ -24,3 +24,36 @@ console.log(jonas);
 const matilda = new Person('Matilda', 2017);
 
 console.log(jonas instanceof Person); // true
+
+////////////////////////////////////////////////////////////
+// Prototypes
+// Each and every function in JavaScript has a property called prototype and that includes of course, constructor functions. Every object that's created by a certain constructor function will get access to all the methods and properties on the constructor prototype properties.
+
+console.log(Person.prototype);
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+// Because we set the method on the prototype of this constructor function, the instances created of that function will inherit said methods.
+jonas.calcAge();
+matilda.calcAge();
+
+// Deprecated __proto__
+console.log(jonas.__proto__);
+
+// Check the prototype with this function:
+console.log(Object.getPrototypeOf(jonas));
+
+// Checking that te protoype of person is the prototype of its 'instances'
+console.log(jonas.__proto__ === Person.prototype); // true
+console.log(Person.prototype.isPrototypeOf(jonas)); // true
+console.log(Person.prototype.isPrototypeOf(Person)); // false
+
+// Its also possible to set properties and not only methods using the prototype:
+Person.prototype.species = 'Homo Sapiens';
+
+console.log(jonas.species, '-----', matilda.species);
+
+// Al the properties inhereted from the prototype are not actually properties of the object. To check said statement:
+console.log(jonas.hasOwnProperty('firstName')); // true (because the constructor function creates this property on the object)
+console.log(jonas.hasOwnProperty('species')); // false
