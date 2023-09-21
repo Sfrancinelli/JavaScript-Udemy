e1efd23194030b8b3ec55251c5f97eb17fb6d87b
unknown <sebastianfrancinelli00@gmail.com>
2023-09-21 11:58:21 -0300

    Lecture 211. Prototype chain on built in objects

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -60,3 +60,35 @@ console.log(jonas.hasOwnProperty('species')); // false
 
 // Prototype of Person is === to Object.__proto__
 console.log(Object.getPrototypeOf(Person) === Object.__proto__); // True
+
+// Prototype of Person
+console.log(jonas.__proto__);
+
+// Prototype of Object
+console.log(jonas.__proto__.__proto__);
+
+// This is null because the prototype of Object is the top of the prototypal chain.
+console.log(jonas.__proto__.__proto__.__proto__); // null
+
+console.log(Person.prototype.constructor);
+
+/////////////////////////////////////////////////
+// Prototype of arrays
+const arr = [3, 4, 5, 6, 7, 8, 3, 8, 6, 3, 3];
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype); // true
+
+// Getting again to the Object.prototype
+console.log(arr.__proto__.__proto__);
+
+// Creating a new method for ALL the arrays in our code. The method its created on the prototype of the Array constructor and that means that every array created will inherit said method.
+Array.prototype.unique = function () {
+  // The this keyword is the array
+  return [...new Set(this)];
+};
+// However... Extending the properties and methods of built in prototypes is a bad practice.
+// 1. Future versions of JS might add a method with the same name but different functions and then the code will do completely different things or break.
+// 2. This is also bad for team working. If every developer on a team proceeds to add methods on the prototype of built in objects, it can lead to serious bugs if there's no proper communication.
+
+// Using the method on the created array
+console.log(arr.unique());
