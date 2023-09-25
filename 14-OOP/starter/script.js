6fd5de321231da41e6d81532265ae167771d00d6
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-09-25 13:03:13 -0300

    CODING CHALLENGE #2

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -253,3 +253,35 @@ steven.calcActualAge();
 // Editing objects programatically and not like above
 const sarah = Object.create(PersonProto);
 sarah.init('Sarah', 1979);
+
+////////////////////////////////////////////////////////////////////7
+// CODING CHALLENGE #2
+class Car2 {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    console.log(`'${this.make}' going at ${this.speed} km/h`);
+  }
+  break() {
+    this.speed -= 5;
+    console.log(`'${this.make}' going at ${this.speed} km/h`);
+  }
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+const ford = new Car2('Ford', 120);
+console.log(ford.speedUS);
+ford.accelerate();
+ford.speedUS = 50; // m/h
+console.log(ford); // 80 km/h
