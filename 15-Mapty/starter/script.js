786b54c7df6c12787550c2563793bb999d6ad1cc
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-09-28 10:49:20 -0300

    Continue working on form submition for the marker to appear

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -3,13 +3,21 @@
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
+// const form = document.querySelector('.form');
+// const containerWorkouts = document.querySelector('.workouts');
+// const inputType = document.querySelector('.form__input--type');
+// const inputDistance = document.querySelector('.form__input--distance');
+// const inputDuration = document.querySelector('.form__input--duration');
+// const inputCadence = document.querySelector('.form__input--cadence');
+// const inputElevation = document.querySelector('.form__input--elevation');
+
 const form = document.querySelector('.form');
-const containerWorkouts = document.querySelector('.workouts');
-const inputType = document.querySelector('.form__input--type');
-const inputDistance = document.querySelector('.form__input--distance');
-const inputDuration = document.querySelector('.form__input--duration');
-const inputCadence = document.querySelector('.form__input--cadence');
-const inputElevation = document.querySelector('.form__input--elevation');
+const formBtn = document.querySelector('.form__btn');
+const formInput = document.querySelector('.form__input--type');
+const distance = document.querySelector('.form__input--distance');
+const duration = document.querySelector('.form__input--duration');
+const cadence = document.querySelector('.form__input--cadence');
+const elevation = document.querySelector('.form__input--elevation');
 
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
@@ -34,6 +42,50 @@ if (navigator.geolocation) {
         const { lat, lng } = mapEvent.latlng;
         console.log(`${lat}, ${lng}`);
 
+        form.classList.remove('hidden');
+
+        form.addEventListener('click', function (e) {
+          e.preventDefault();
+          if (formInput.value === 'cycling') {
+            cadence.parentNode.classList.add('form__row--hidden');
+            elevation.parentNode.classList.remove('form__row--hidden');
+          }
+
+          if (formInput.value === 'running') {
+            cadence.parentNode.classList.remove('form__row--hidden');
+            elevation.parentNode.classList.add('form__row--hidden');
+          }
+        });
+      });
+
+      let className;
+      let content;
+
+      form.addEventListener('submit', function (e) {
+        console.log(formInput.value);
+        console.log(distance.value);
+        console.log(duration.value);
+        console.log(cadence.value);
+        console.log(elevation.value);
+
+        const distance = distance.value;
+        const duration = duration.value;
+        const cadence = cadence.value;
+        const elevation = elevation.value;
+
+        const date = new Date();
+        const dd = String(today.getDate()).padStart(2, '0');
+        const mm = String(today.getMonth() + 1).padStart(2, '0');
+
+        if (formInput.value === 'cycling') {
+          className = 'cycling-popup';
+          content = `🚴‍♂️ Cycling on ${mm} ${dd}`;
+        }
+        if (formInput.value === 'running') {
+          className = 'running-popup';
+          content = `🏃‍♂️ Running on ${mm} ${dd}`;
+        }
+
         // Setting a marker
         L.marker([lat, lng])
           .addTo(map)
@@ -43,12 +95,15 @@ if (navigator.geolocation) {
               minWidth: 100,
               autoClose: false,
               closeOnClick: false,
-              className: 'running-popup',
+              className: `${className}`,
             })
           )
-          .setPopupContent("🏃‍♂️ Running on 'Date'")
+          .setPopupContent(`${content}`)
           .openPopup();
+        console.log(e);
+        e.preventDefault();
       });
+
       // console.log(map);
     },
     function () {
