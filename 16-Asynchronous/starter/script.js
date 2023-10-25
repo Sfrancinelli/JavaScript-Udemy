4c1066d86ce33f6f83eed6385027bc4b7f98e1ae
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-10-25 13:13:47 -0300

    Running promises in parallel (Promise.all())

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -664,3 +664,29 @@ try {
 } catch (err) {
   console.error(err.message);
 }
+
+////////////////////////////////////////////////////////////////
+// Running promises in parallel
+
+const get3Countries = async function (c1, c2, c3) {
+  try {
+    // const [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
+    // const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
+    // const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
+
+    // To run promises in parallel we use the Promise.all() method:
+    const data = await Promise.all([
+      getJSON(`https://restcountries.com/v3.1/name/${c1}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c2}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c3}`),
+    ]);
+
+    console.log(data.map(d => d[0].capital[0]));
+
+    // console.log([...data1.capital, ...data2.capital, ...data3.capital]);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+get3Countries('portugal', 'canada', 'tanzania');
