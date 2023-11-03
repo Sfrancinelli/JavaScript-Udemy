7033598ea83878a457bfc8c0a826d63037df6f7b
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-11-03 09:27:29 -0300

    top level await on modules

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -8,13 +8,52 @@
 // addToCart('bread', 5);
 // console.log(`price: ${price}, quantity: ${quan}`);
 
-import * as ShoppingCart from './shoppingCart.js';
+// import * as ShoppingCart from './shoppingCart.js';
 
-console.log('Importing moduel');
-console.log(ShoppingCart);
-ShoppingCart.addToCart('bread', 5);
-console.log(ShoppingCart.totalPrice);
+// console.log('Importing moduel');
+// console.log(ShoppingCart);
+// ShoppingCart.addToCart('bread', 5);
+// console.log(ShoppingCart.totalPrice);
 
 // console.log(shippingCost) // Not defined because variables are module scoped if not exported
-import add from './shoppingCart.js';
-// Importing the default export
+// import add from './shoppingCart.js';
+// // Importing the default export no matter how the default export its called (in this case it doesnt have a name)
+// add('pizza', 2);
+
+import add, {
+  addToCart,
+  totalPrice as price,
+  totalQuantity as tq,
+  cart,
+} from './shoppingCart.js';
+add('pizza', 2);
+console.log(price);
+
+add('bread', 5);
+add('apples', 4);
+
+console.log(cart); // Prove that the import/export its a live connection. The cart was an empty array when exported and now, when logged to the console after adding the objects, it changed.
+
+// Top level await blocks the execution of the rest of the code
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+// const data = await res.json();
+// console.log(data);
+
+// console.log('Something')
+
+const getLastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+  // console.log(data);
+
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+const lastPost = getLastPost(); // Returns promise
+
+// NOt very clean
+lastPost.then(last => console.log(last));
+
+// Consuming the promise with top level await
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
--- 17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
+++ 17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
@@ -1,8 +1,16 @@
 // Exporting module
 console.log('Exporting module');
 
+// Blocking code
+// The code in script js will need to await for the promise to be fullfilled as we are using top level await and its blocking the code in this script but also on the script that imports it
+console.log('Start fetching users');
+const res = await fetch('https://jsonplaceholder.typicode.com/users');
+const data = await res.json();
+console.log(data);
+console.log('Finished fetching users');
+
 const shippingCost = 10;
-const cart = [];
+export const cart = [];
 
 // Exports need to happen on top level code (cant export on block scope)
 export const addToCart = function (product, quantity) {
