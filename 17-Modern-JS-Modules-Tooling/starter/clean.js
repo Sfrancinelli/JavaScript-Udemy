25e08e296a0eb2c144b41081604c269ed2165aaf
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-11-09 11:32:32 -0300

    Cleaning the code od clean.js and making it functional programming

--- 17-Modern-JS-Modules-Tooling/starter/clean.js
+++ 17-Modern-JS-Modules-Tooling/starter/clean.js
@@ -1,3 +1,5 @@
+'strict mode';
+
 // Fixing bad practices on code
 
 /* BAD
@@ -12,7 +14,9 @@ var budget = [
   { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
 ];
 */
-const budget = [
+
+// Freezing the budget cause its a good practice to not modify the original data and create copies of this data and then modify said copies
+const budget = Object.freeze([
   { value: 250, description: 'Sold old TV 📺', user: 'jonas' },
   { value: -45, description: 'Groceries 🥑', user: 'jonas' },
   { value: 3500, description: 'Monthly salary 👩‍💻', user: 'jonas' },
@@ -21,7 +25,7 @@ const budget = [
   { value: -20, description: 'Candy 🍭', user: 'matilda' },
   { value: -125, description: 'Toys 🚂', user: 'matilda' },
   { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
-];
+]);
 
 /* BAD
 var limits = {
@@ -30,10 +34,14 @@ var limits = {
 };
 */
 
-const limits = {
+// By wrapping the limits into the Object.freeze function we make the object inmutable
+const limits = Object.freeze({
   jonas: 1500,
   matilda: 100,
-};
+});
+// This wont work cause of the Object.freeze()
+limits.jay = 200;
+console.log(limits);
 
 /* BAD
 var add = function (value, description, user) {
@@ -57,23 +65,41 @@ const checkUser = function (user) {
   return user.toLowerCase();
 };
 
-const checkLimit = function (user) {
+const checkLimit = function (limits, user) {
   return limits[user] ? limits[user] : 0;
 };
 
-const addExpense = function (value, description, user = 'jonas') {
-  user = checkUser(user);
+const addExpense = function (
+  state,
+  limits,
+  value,
+  description,
+  user = 'jonas'
+) {
+  const cleanUser = checkUser(user);
 
-  const limit = checkLimit(user);
+  const limit = checkLimit(limits, cleanUser);
 
   if (value <= limit) {
-    budget.push({ value: -value, description, user });
+    // Instead of pushing into the original data, its a better practice to create a copy of the data and modify said copy
+    return [...state, { value: -value, description, user: cleanUser }];
+
+    // budget.push({ value: -value, description, user: cleanUser });
   }
+
+  return state;
 };
 
-addExpense(10, 'Pizza 🍕');
-addExpense(100, 'Going to movies 🍿', 'Matilda');
-addExpense(200, 'Stuff', 'Jay');
+const newBudget1 = addExpense(budget, limits, 10, 'Pizza 🍕');
+console.log(newBudget1);
+const newBudget2 = addExpense(
+  newBudget1,
+  limits,
+  100,
+  'Going to movies 🍿',
+  'Matilda'
+);
+const newBudget3 = addExpense(newBudget2, limits, 200, 'Stuff', 'Jay');
 
 /* BAD
 var checkBudget = function () {
@@ -95,23 +121,39 @@ checkBudget();
 console.log(budget);
 */
 
-const checkBudget = function () {
-  budget.forEach(el => {
-    if (el.value < checkLimit(el.user)) el.flag = 'limit';
+const checkBudget = function (state, limit) {
+  return state.map(entry => {
+    return entry.value < -checkLimit(limit, entry.user)
+      ? { ...entry, flag: 'limit' }
+      : entry;
   });
-};
-checkBudget();
 
-const bigExpenses = function (bigLimit) {
-  let output = '';
-  for (const entry of budget) {
-    output +=
-      entry.value <= -bigLimit ? `${entry.description.slice(-2)} / ` : '';
-  }
-  output = output.slice(0, -2); // Remove last '/ '
-  console.log(output);
+  // return state.map(el => {
+  //   if (el.value < checkLimit(limit, el.user)) el.flag = 'limit';
+  // });
+};
+const finalBudget = checkBudget(newBudget3, limits);
+console.log(finalBudget);
+
+// const bigExpenses = function (state, bigLimit) {
+//   let output = '';
+//   for (const entry of budget) {
+//     output +=
+//       entry.value <= -bigLimit ? `${entry.description.slice(-2)} / ` : '';
+//   }
+//   output = output.slice(0, -2); // Remove last '/ '
+//   console.log(output);
+// };
+
+const bigExpenses = function (state, bigLimit) {
+  const bigExpenses = state
+    .filter(entry => entry.value <= -bigLimit)
+    .map(entry => entry.description.slice(-2))
+    .join(' / ');
+
+  return bigExpenses;
 };
 
-bigExpenses(1000);
-bigExpenses(100);
-console.log(budget);
+console.log(bigExpenses(finalBudget, 1000));
+bigExpenses(finalBudget, 100);
+console.log(finalBudget);
