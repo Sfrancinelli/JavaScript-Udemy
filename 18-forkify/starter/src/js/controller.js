d735ef0d55850025e9c4bf258b6fb93d95b094fe
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-11-09 16:30:53 -0300

    working with the forkify api and formatting the response

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -8,6 +8,39 @@ const timeout = function (s) {
   });
 };
 
+console.log('TEST');
+
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcc40'
+    );
+
+    const data = await res.json();
+
+    if (!res.ok)
+      throw new Error(`${data.message} - Status code: (${res.status})`);
+
+    let { recipe } = data.data;
+    recipe = {
+      title: recipe.title,
+      cookingTime: recipe.cooking_time,
+      id: recipe.id,
+      imageUrl: recipe.image_url,
+      ingredients: recipe.ingredients,
+      publisher: recipe.publisher,
+      servings: recipe.servings,
+      sourceUrl: recipe.source_url,
+    };
+
+    console.log(recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+showRecipe();
