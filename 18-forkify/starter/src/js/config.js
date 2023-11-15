36416fcf420cdc054f45690623fc368bbb3e2d73
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-11-15 17:23:11 -0300

    Added function to manage querys and values returned from query

--- 18-forkify/starter/src/js/config.js
+++ 18-forkify/starter/src/js/config.js
@@ -1,4 +1,4 @@
 // This file will contain constant variables and pieces of code that will be used multiple times
 
-export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SECS = 10;
--- 18-forkify/starter/src/js/model.js
+++ 18-forkify/starter/src/js/model.js
@@ -3,11 +3,15 @@ import { getJSON } from './helpers.js';
 
 export const state = {
   recipe: {},
+  search: {
+    query: '',
+    results: [],
+  },
 };
 
 export const loadRecipe = async function (id) {
   try {
-    const data = await getJSON(`${API_URL}/${id}`);
+    const data = await getJSON(`${API_URL}${id}`);
 
     const { recipe } = data.data;
     state.recipe = {
@@ -21,6 +25,30 @@ export const loadRecipe = async function (id) {
       sourceUrl: recipe.source_url,
     };
   } catch (err) {
+    console.error(`${err} 💥`);
     throw err;
   }
 };
+
+export const loadSearchResults = async function (query) {
+  try {
+    state.search.query = query;
+    const data = await getJSON(`${API_URL}?search=${query}`);
+    console.log(data);
+
+    state.search.results = data.data.recipes.map(rec => {
+      return {
+        id: rec.id,
+        title: rec.title,
+        publisher: rec.publisher,
+        imageUrl: rec.image_url,
+      };
+    });
+    console.log(state.search.results);
+  } catch (err) {
+    console.error(`${err} 💥`);
+    throw err;
+  }
+};
+
+loadSearchResults('pizza');
