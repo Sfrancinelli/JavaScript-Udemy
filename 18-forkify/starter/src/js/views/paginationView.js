32789aec9bd321390214161e9eacd401fb5563d5
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-11-21 14:39:33 -0300

    rendring the pages according to different conditions

--- 18-forkify/starter/src/js/views/paginationView.js
+++ 18-forkify/starter/src/js/views/paginationView.js
@@ -7,14 +7,57 @@ class PaginationView extends View {
   _parentEl = document.querySelector('.pagination');
 
   _generateMarkup() {
-    const numPages =
-      Math.trunc(this._data.results.length / this._data.resultsPerPage) + 1;
+    const curPage = this._data.page;
+
+    const numPages = Math.ceil(
+      this._data.results.length / this._data.resultsPerPage
+    );
     console.log(numPages);
 
     // Page 1, and there are other pages
-    // Page 1, and there are NO other pages
+    if (curPage === 1 && numPages > 1) {
+      return `
+        <button class="btn--inline pagination__btn--next">
+            <span>Page ${curPage + 1}</span>
+            <svg class="search__icon">
+              <use href="${icons}#icon-arrow-right"></use>
+            </svg>
+        </button>
+      `;
+    }
+
     // Last page
+    if (curPage === numPages && numPages > 1) {
+      return `
+        <button class="btn--inline pagination__btn--prev">
+            <svg class="search__icon">
+              <use href="${icons}#icon-arrow-left"></use>
+            </svg>
+            <span>Page ${curPage - 1}</span>
+        </button>
+      `;
+    }
+
     // Other page (prev and next btns)
+    if (curPage < numPages) {
+      return `
+        <button class="btn--inline pagination__btn--prev">
+            <svg class="search__icon">
+              <use href="${icons}#icon-arrow-left"></use>
+            </svg>
+            <span>Page ${curPage - 1}</span>
+        </button>
+        <button class="btn--inline pagination__btn--next">
+            <span>Page ${curPage + 1}</span>
+            <svg class="search__icon">
+                <use href="${icons}#icon-arrow-right"></use>
+            </svg>
+        </button>
+      `;
+    }
+
+    // Page 1, and there are NO other pages
+    return '';
   }
 }
 
