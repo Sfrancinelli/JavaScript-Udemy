4bc301d5efeda9744ccf2ccd6131911a572d583c
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-11-21 11:03:14 -0300

    refactor ResultView to make it inherit from View.js

--- 18-forkify/starter/src/js/views/resultsView.js
+++ 18-forkify/starter/src/js/views/resultsView.js
@@ -1,42 +1,42 @@
+import View from './View.js';
+
 import icons from 'url:../../img/icons.svg'; // Importing icons from dist
 import { Fraction } from 'fractional';
 
-class ResultView {
-  #parentEl = document.querySelector('.results');
-  #data;
-  #errorMessage = 'We could not find that recipe. Please try another one!';
-  #message = '';
+class ResultView extends View {
+  _parentEl = document.querySelector('.results');
+  _errorMessage = 'No recipes found for that query!. Please try another one!';
+  _message = '';
 
-  render(data) {
-    console.log(data);
-    this.#data = data;
-    this.#clear();
-    this.#data.forEach(result => {
-      const markup = this.#generateMarkupResults(result);
-      this.#parentEl.insertAdjacentHTML('afterbegin', markup);
-    });
+  // render(data) {
+  //   console.log(data);
+  //   this._data = data;
+  //   this._clear();
+  //   this._data.forEach(result => {
+  //     const markup = this._generateMarkupResults(result);
+  //     this._parentEl.insertAdjacentHTML('afterbegin', markup);
+  //   });
+  // }
+
+  _generateMarkup() {
+    return this._data.map(this._generateMarkupPreview).join('');
   }
 
-  #generateMarkupResults(result) {
-    console.log(result);
+  _generateMarkupPreview(result) {
     return `
     <li class="preview">
-    <a class="preview__link preview__link--active" href="#${result.id}">
-      <figure class="preview__fig">
-        <img src="${result.imageUrl}" alt="Preview image of the recipe" />
-      </figure>
-      <div class="preview__data">
-        <h4 class="preview__title">${result.title}</h4>
-        <p class="preview__publisher">${result.publisher}</p>
+      <a class="preview__link" href="#${result.id}">
+        <figure class="preview__fig">
+          <img src="${result.imageUrl}" alt="${result.title}" />
+        </figure>
+        <div class="preview__data">
+          <h4 class="preview__title">${result.title}</h4>
+          <p class="preview__publisher">${result.publisher}</p>
+          </div>
         </div>
-      </div>
-    </a>
-  </li>
-    `;
-  }
-
-  #clear() {
-    this.#parentEl.innerHTML = '';
+      </a>
+    </li>
+  `;
   }
 }
 
