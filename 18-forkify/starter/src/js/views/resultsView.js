6bce25215ea1de4a786f54322289cab2b2724d5e
Sfrancinelli <sebastianfrancinelli00@gmail.com>
2023-11-24 12:43:38 -0300

    using the previewView to generate the markup in the results view

--- 18-forkify/starter/src/js/views/resultsView.js
+++ 18-forkify/starter/src/js/views/resultsView.js
@@ -1,4 +1,6 @@
 import View from './View.js';
+import previewView from './previewView.js';
+
 import { Fraction } from 'fractional';
 class ResultView extends View {
   _parentEl = document.querySelector('.results');
@@ -6,28 +8,7 @@ class ResultView extends View {
   _message = '';
 
   _generateMarkup() {
-    return this._data.map(this._generateMarkupPreview).join('');
-  }
-
-  _generateMarkupPreview(result) {
-    const id = window.location.hash.slice(1);
-
-    return `
-    <li class="preview">
-      <a class="preview__link ${
-        result.id === id ? 'preview__link--active' : ''
-      }" href="#${result.id}">
-        <figure class="preview__fig">
-          <img src="${result.imageUrl}" alt="${result.title}" />
-        </figure>
-        <div class="preview__data">
-          <h4 class="preview__title">${result.title}</h4>
-          <p class="preview__publisher">${result.publisher}</p>
-          </div>
-        </div>
-      </a>
-    </li>
-  `;
+    return this._data.map(result => previewView.render(result, false)).join('');
   }
 }
 
